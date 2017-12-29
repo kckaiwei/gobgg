c19d547292e3442aed7f84e3125a9783a2af13b3
Kevin Chang <kckaiwei@gmail.com>
2017-12-29 13:33:42 -0600

    Adding other suffixes

--- config.go
+++ config.go
@@ -2,5 +2,15 @@ package gobgg
 
 const (
 	BaseURL = "https://www.boardgamegeek.com/xmlapi2/"
+	CollectionSuffix = "collection"
+	FamilySuffix = "family"
+	ForumListSuffix = "forumlist"
+	ForumsSuffix = "forum"
+	GuildSuffix = "guild"
+	HotSuffix = "hot"
+	PlaySuffix = "plays"
+	SearchSuffix = "search"
+	ThingSuffix = "thing"
+	ThreadsSuffix = "thread"
 	UserSuffix = "user"
 )
